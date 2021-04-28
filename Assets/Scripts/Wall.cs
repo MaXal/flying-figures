23b239d7063c7f3b4fcf2572015b3c6ad35c336a
Maxim.Kolmakov <maxim.kolmakov@jetbrains.com>
2021-04-28 16:32:57 +0200

    Generate wall

--- Assets/Scripts/Wall.cs
+++ Assets/Scripts/Wall.cs
@@ -3,6 +3,17 @@ using UnityEngine;
 public class Wall : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 1f;
+    [SerializeField] private GameObject tile;
+
+    private void Start()
+    {
+        for (var i = 0; i < 9; i++)
+        {
+            var generatedTile = Instantiate(tile, new Vector3(transform.position.x, 1 + i * 2, 0),
+                Quaternion.Euler(0, 0, 90));
+            generatedTile.transform.parent = gameObject.transform;
+        }
+    }
 
     private void Update()
     {
