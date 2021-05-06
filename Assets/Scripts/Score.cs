33c50b53a47fe7f24211b17f9ba58c535d2b5af6
Aleksey Papushin <aleks.papushin@gmail.com>
2021-05-06 21:02:02 +0300

    GameTime replaced by Score. Player got 1 score per each wall passing

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,25 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class Score : MonoBehaviour
+{
+    private TextMeshProUGUI text;
+    private int score;
+
+    private void Start()
+    {
+        text = GetComponent<TextMeshProUGUI>();
+        FindObjectOfType<Player>().OnPlayerPassedWall += OnPlayerPassedWall;
+
+    }
+
+    private void Update()
+    {
+        text.text = score.ToString();
+    }
+
+    private void OnPlayerPassedWall()
+    {
+        score++;
+    }
+}
